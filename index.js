f9c84699bedb2e78a94ffec7d160dad552694d25
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2019-10-15 16:29:28 -0400

    better error handling

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const contentTypes = {
 
 const cleanText = s => s.trim().replace(/\s\s+/g, ' ')
 
+const formatJSON = (obj, pretty) => JSON.stringify(obj, null, pretty ? 2 : 0)
+
+const generateJSONResponse = (obj, pretty) => {
+  return new Response(formatJSON(obj, pretty), {
+    headers: { 'content-type': contentTypes.json }
+  })
+}
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -18,90 +26,98 @@ async function handleRequest(request) {
   if (url && !url.match(/^[a-zA-Z]+:\/\//)) url = 'http://' + url
 
   const selector = searchParams.get('selector')
-  const pretty = searchParams.get('pretty')
   const spaced = searchParams.get('spaced') // Adds spaces between tags
+  const pretty = searchParams.get('pretty')
 
-  if (url && selector) {
-    const response = await fetch(url)
+  if (!url || !selector) {
+    return handleSiteRequest(request)
+  }
 
-    const rewriter = new HTMLRewriter()
+  return handleAPIRequest({ url, selector, spaced, pretty })
+}
 
-    const matches = {}
-    const selectors = selector.split(',').map(s => s.trim())
+async function handleSiteRequest(request) {
+  const url = new URL(request.url)
 
-    selectors.forEach((selector) => {
-      matches[selector] = []
-
-      let nextText = ''
-
-      rewriter.on(selector, {
-        element(element) {
-          matches[selector].push(true)
-          nextText = ''
-        },
-
-        text(text) {
-          nextText += text.text
-
-          if (text.lastInTextNode) {
-            if (spaced) nextText += ' '
-            matches[selector].push(nextText)
-            nextText = ''
-          }
-        }
-      })
-    })
-
-    const transformed = rewriter.transform(response)
-
-    await transformed.text()
-
-    selectors.forEach((selector) => {
-      const nodeCompleteTexts = []
-
-      let nextText = ''
-
-      matches[selector].forEach(text => {
-        if (text === true) {
-          if (nextText.trim() !== '') {
-            nodeCompleteTexts.push(cleanText(nextText))
-            nextText = ''
-          }
-        } else {
-          nextText += text
-        }
-      })
-
-      const lastText = cleanText(nextText)
-      if (lastText !== '') nodeCompleteTexts.push(lastText)
-      matches[selector] = nodeCompleteTexts
-    })
-
-    const json = {
-      result: selectors.length === 1 ? matches[selectors[0]] : matches
-    }
-
-    const jsonString = JSON.stringify(json, null, pretty ? 2 : 0)
-
-    return new Response(jsonString, {
-      headers: { 'content-type': contentTypes.json }
-    })
-
-  } else {
-    const url = new URL(request.url)
-
-    if (url.pathname === '/' || url.pathname === '') {
-      return new Response(html, {
-        headers: { 'content-type': contentTypes.html }
-      })
-
-    } else {
-      // TODO - handle other paths?
-      return new Response('Not found', { status: 404 })
-    }
-
-    addEventListener('fetch', event => {
-      event.respondWith(handleRequest(event.request))
+  if (url.pathname === '/' || url.pathname === '') {
+    return new Response(html, {
+      headers: { 'content-type': contentTypes.html }
     })
   }
+
+  return new Response('Not found', { status: 404 })
+}
+
+async function handleAPIRequest({ url, selector, spaced, pretty }) {
+  const response = await fetch(url)
+
+  const server = response.headers.get('server')
+
+  const isThisWorkerErrorNotErrorWithinScrapedSite = (
+    [530, 503, 502, 403].includes(response.status) &&
+    (server === 'cloudflare' || !server /* Workers preview editor */)
+  )
+
+  if (isThisWorkerErrorNotErrorWithinScrapedSite) {
+    return generateJSONResponse({
+      error: `Status ${ response.status } requesting ${ url }`
+    }, pretty)
+  }
+
+  const rewriter = new HTMLRewriter()
+
+  const matches = {}
+  const selectors = selector.split(',').map(s => s.trim())
+
+  selectors.forEach((selector) => {
+    matches[selector] = []
+
+    let nextText = ''
+
+    rewriter.on(selector, {
+      element(element) {
+        matches[selector].push(true)
+        nextText = ''
+      },
+
+      text(text) {
+        nextText += text.text
+
+        if (text.lastInTextNode) {
+          if (spaced) nextText += ' '
+          matches[selector].push(nextText)
+          nextText = ''
+        }
+      }
+    })
+  })
+
+  const transformed = rewriter.transform(response)
+
+  await transformed.text()
+
+  selectors.forEach((selector) => {
+    const nodeCompleteTexts = []
+
+    let nextText = ''
+
+    matches[selector].forEach(text => {
+      if (text === true) {
+        if (nextText.trim() !== '') {
+          nodeCompleteTexts.push(cleanText(nextText))
+          nextText = ''
+        }
+      } else {
+        nextText += text
+      }
+    })
+
+    const lastText = cleanText(nextText)
+    if (lastText !== '') nodeCompleteTexts.push(lastText)
+    matches[selector] = nodeCompleteTexts
+  })
+
+  return generateJSONResponse({
+    result: selectors.length === 1 ? matches[selectors[0]] : matches
+  }, pretty)
 }
