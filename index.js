b09d3dab8672cdcbd51a3cf116d08c4e5054f1f9
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-02-25 19:06:16 -0500

    refactor
    - new Scraper class
    - more detailed error handling
    - support attribute scraping (API-only for now)

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 import html from './html.js'
-
-const contentTypes = {
-  html: 'text/html;charset=UTF-8',
-  json: 'application/json;charset=UTF-8'
-}
-
-const cleanText = s => s.trim().replace(/\s\s+/g, ' ')
-
-const formatJSON = (obj, pretty) => JSON.stringify(obj, null, pretty ? 2 : 0)
-
-const generateJSONResponse = (obj, pretty) => {
-  return new Response(formatJSON(obj, pretty), {
-    headers: {
-      'content-type': contentTypes.json,
-      'Access-Control-Allow-Origin': '*'
-    }
-  })
-}
+import contentTypes from './content-types.js'
+import Scraper from './scraper.js'
+import { generateJSONResponse, generateErrorJSONResponse } from './json-response.js'
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -29,6 +14,7 @@ async function handleRequest(request) {
   if (url && !url.match(/^[a-zA-Z]+:\/\//)) url = 'http://' + url
 
   const selector = searchParams.get('selector')
+  const attr = searchParams.get('attr')
   const spaced = searchParams.get('spaced') // Adds spaces between tags
   const pretty = searchParams.get('pretty')
 
@@ -36,7 +22,7 @@ async function handleRequest(request) {
     return handleSiteRequest(request)
   }
 
-  return handleAPIRequest({ url, selector, spaced, pretty })
+  return handleAPIRequest({ url, selector, attr, spaced, pretty })
 }
 
 async function handleSiteRequest(request) {
@@ -51,83 +37,25 @@ async function handleSiteRequest(request) {
   return new Response('Not found', { status: 404 })
 }
 
-async function handleAPIRequest({ url, selector, spaced, pretty }) {
-  const response = await fetch(url)
-
-  const server = response.headers.get('server')
-
-  const isThisWorkerErrorNotErrorWithinScrapedSite = (
-    [530, 503, 502, 403, 400].includes(response.status) &&
-    (server === 'cloudflare' || !server /* Workers preview editor */)
-  )
-
-  if (isThisWorkerErrorNotErrorWithinScrapedSite) {
-    return generateJSONResponse({
-      error: `Status ${ response.status } requesting ${ url }`
-    }, pretty)
-  }
-
-  const rewriter = new HTMLRewriter()
-
-  const matches = {}
-  const selectors = new Set(selector.split(',').map(s => s.trim()))
+async function handleAPIRequest({ url, selector, attr, spaced, pretty }) {
+  let scraper, result
 
   try {
-    selectors.forEach((selector) => {
-      matches[selector] = []
-
-      let nextText = ''
-
-      rewriter.on(selector, {
-        element(element) {
-          matches[selector].push(true)
-          nextText = ''
-        },
-
-        text(text) {
-          nextText += text.text
-
-          if (text.lastInTextNode) {
-            if (spaced) nextText += ' '
-            matches[selector].push(nextText)
-            nextText = ''
-          }
-        }
-      })
-    })
-
+    scraper = await new Scraper().fetch(url)
   } catch (error) {
-    return generateJSONResponse({
-        error: `The selector \`${ selector }\` is invalid or another HTML parsing error occured`
-    }, pretty)
+    return generateErrorJSONResponse(error, pretty)
   }
 
-  const transformed = rewriter.transform(response)
+  try {
+    if (!attr) {
+      result = await scraper.querySelector(selector).getText({ spaced })
+    } else {
+      result = await scraper.querySelector(selector).getAttribute(attr)
+    }
 
-  await transformed.text()
+  } catch (error) {
+    return generateErrorJSONResponse(error, pretty)
+  }
 
-  selectors.forEach((selector) => {
-    const nodeCompleteTexts = []
-
-    let nextText = ''
-
-    matches[selector].forEach(text => {
-      if (text === true) {
-        if (nextText.trim() !== '') {
-          nodeCompleteTexts.push(cleanText(nextText))
-          nextText = ''
-        }
-      } else {
-        nextText += text
-      }
-    })
-
-    const lastText = cleanText(nextText)
-    if (lastText !== '') nodeCompleteTexts.push(lastText)
-    matches[selector] = nodeCompleteTexts
-  })
-
-  return generateJSONResponse({
-    result: selectors.length === 1 ? matches[selectors[0]] : matches
-  }, pretty)
+  return generateJSONResponse({ result }, pretty)
 }
